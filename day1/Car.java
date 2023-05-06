f68a82f83d87602523930e451a3f2fa19276ea58
janatsunanda24 <132730215+janatsunanda24@users.noreply.github.com>
2023-05-06 10:02:59 +0530

    Add files via upload
    week1Day1

---
+++ day1/Car.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class Car {
+
+	public void fastenSeatBelt() {
+		System.out.println("fastenSeatBelt");
+	}
+	public void startTheCar() {
+		System.out.println("startTheCar");
+	}
+	public void releaseHandBreak() {
+		System.out.println("releaseHandBreak");
+	}
+	public void turnOnAC() {
+		System.out.println("turnOnAC");
+	}	
+	public void applyBreak() {
+		System.out.println("applyBreak");
+	}
+	
+		
+	
+	public static void main(String[] args) {
+	    Car kia=new Car();
+	    kia.fastenSeatBelt();
+	    kia.startTheCar();
+	    kia.releaseHandBreak();
+	    kia.turnOnAC();
+	    kia.applyBreak();
+
+	}
+
+}
+
+
---
+++ day1/Mobile.java
@@ -0,0 +1,42 @@
+package week1.day1;
+
+public class Mobile {
+	public void capturePhoto() {
+		System.out.println("capturePhoto");
+	}
+	public void sendMoney() {
+		System.out.println("sendMoney");
+	}
+	public void sendSMS() {
+		System.out.println("sendSMS");
+	}
+	public void switchOnFlashLIght() {
+		System.out.println("switchOnFlashLight");
+	}
+	public void watchVideo() {
+		System.out.println("watchVideo");
+	}
+	
+	
+
+	public static void main(String[] args) {
+	   Mobile apple=new Mobile();
+	   apple.capturePhoto();
+	   apple.sendMoney();
+	   apple.switchOnFlashLIght();
+	   apple.watchVideo();
+	   
+
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	}
+
+}
