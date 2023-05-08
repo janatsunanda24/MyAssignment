51d8e55345fd5bbaba088b4d7656add3af079a94
janatsunanda24 <132730215+janatsunanda24@users.noreply.github.com>
2023-05-08 22:04:59 +0530

    Add files via upload

---
+++ twoWheeler.java
@@ -0,0 +1,31 @@
+package week1day2;
+
+public class twoWheeler {
+	 int noOfWheels=2;
+	 short noOfMirrors=2;
+	 long chassisNumber=6543;
+	 boolean isPunctured=true;
+	 String bikeName="Bullet";
+	 double runningKM=378.5;
+	 
+	public static void main(String[] args) {
+		twoWheeler Bullet=new twoWheeler();
+   
+    System.out.println(Bullet.noOfWheels);
+    
+    System.out.println(Bullet.noOfMirrors);
+    
+    System.out.println(Bullet.chassisNumber);
+    
+    System.out.println(Bullet.isPunctured);
+    
+    System.out.println(Bullet.bikeName);
+    
+    System.out.println(Bullet.runningKM);
+    
+    
+    
+
+	}
+
+}
